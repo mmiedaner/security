bbd59159be5381567d653ed765d308d8e81b76d0
mmiedaner <markusmiedaner@gmail.com>
2015-11-09 17:29:25 +0100

    Update regexFuzzer.py

--- fuzz-files/tools/regexFuzzer.py
+++ fuzz-files/tools/regexFuzzer.py
@@ -1,8 +1,9 @@
 import rstr
 import argparse
 
-parser = argparse.ArgumentParser(description='Create fuzzing strings based on regex.')
-parser.add_argument('maxCycles', metavar='count', type=int, help='number of fuzzing strings to be generated')
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('maxCycles', metavar='M', type=int, 
+                   help='number of fuzzing strings to be generated')
 parser.add_argument('--regex', metavar='regex', help='regular expression as string')
 parser.add_argument('--out', metavar='file', help='file to write output to')
 args = parser.parse_args()
@@ -10,9 +11,8 @@ args = parser.parse_args()
 target = open(args.out, 'w')
 
 counter = 0
-while (counter < args.maxCycles[0]):
+while (counter < args.maxCycles):
     result = rstr.xeger(args.regex)
-    print result
     target.write(result)
     target.write('\n')
     counter = counter+1
