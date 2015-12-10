95cad38d63360f2e70ddf1a9a7bfd50bc25fb8f7
mmiedaner <markusmiedaner@gmail.com>
2015-12-10 15:41:59 +0100

    improvements and clean up

--- metric/SonarMetricsExtractor.py
+++ metric/SonarMetricsExtractor.py
@@ -4,24 +4,24 @@ import argparse
 import sys
 
 
-def writeStatusMsg(id, i, maximum):
-    sys.stdout.write('\r Collecting issues for key: ' + id + ' - page ' + str(i) + ' of ' + str(maximum))
+def write_statusmessage(messageid, i, maximum):
+    sys.stdout.write('\r Collecting issues for key: ' + messageid + ' - page ' + str(i) + ' of ' + str(maximum))
     sys.stdout.flush()
     return
 
 
-def makeApiCall(connection, method, apiString):
-    connection.request(method, apiString)
+def make_apicall(connection, method, apistring):
+    connection.request(method, apistring)
     resp = connection.getresponse()
     data = json.load(resp)
     return data
 
 
-def getRuleKeys(connection):
+def get_rulekeys(connection):
     url = '/sonar/api/profiles?language=' + language
-    result = makeApiCall(connection, "GET", url)
+    service_answer = make_apicall(connection, "GET", url)
     squid = []
-    for profile in result:
+    for profile in service_answer:
         if 'rules' in profile:
             for rule in profile['rules']:
                 for repos in repo:
@@ -30,32 +30,32 @@ def getRuleKeys(connection):
     return squid
 
 
-def getIssuesForSquid(connection, id):
-    url = "/sonar/api/issues/search?squid=" + id + "&pageSize=-1"
-    result = makeApiCall(connection, "GET", url)
-    issueList = []
-    if 'issues' in result:
-        for issue in result['issues']:
-            issueList.append(issue)
+def get_issues_for_squid(connection, squid_id):
+    url = "/sonar/api/issues/search?squid=" + squid_id + "&pageSize=-1"
+    service_answer = make_apicall(connection, "GET", url)
+    issuelist = []
+    if 'issues' in service_answer:
+        for issue in service_answer['issues']:
+            issuelist.append(issue)
 
-    if 'maxResultsReached' in result and result['maxResultsReached']:
-        paging = result['paging']
+    if 'maxResultsReached' in service_answer and service_answer['maxResultsReached']:
+        paging = service_answer['paging']
         i = paging['pageIndex']
         while i < paging['pages']:
             i += 1
-            writeStatusMsg(id, i, paging['pages'])
-            result2 = makeApiCall(connection, "GET", url + "&pageIndex=" + str(i))
+            write_statusmessage(squid_id, i, paging['pages'])
+            result2 = make_apicall(connection, "GET", url + "&pageIndex=" + str(i))
             if 'issues' in result2:
                 for issue in result2['issues']:
-                    issueList.append(issue)
+                    issuelist.append(issue)
 
-    if len(issueList) > 0:
-        return issueList
+    if len(issuelist) > 0:
+        return issuelist
 
-    return "nix"
+    return None
 
 
-def mapArgsToGloablVars(args):
+def map_args_to_global_vars(args):
     if args.out:
         global fileName
         fileName = args.out
@@ -72,64 +72,80 @@ def mapArgsToGloablVars(args):
         global mainURL
         mainURL = args.baseurl
 
+    global separator
+    if args.separator:
+        separator = args.separator
+    else:
+        separator = "\t"
 
-def parseCommandlineParamters():
+
+def parse_commandline_paramters():
     parser = argparse.ArgumentParser(
         description='Collect issue statistics from sonarquebe based on repositories of rules.')
     parser.add_argument('--out', metavar='file', help='file to write output to')
     parser.add_argument('--repo', metavar='repo', help='name of repository to query', nargs='+')
     parser.add_argument('--lang', metavar='lang', help='progamming language used in projects')
     parser.add_argument('--baseurl', metavar='burl', help='base url of sonarqube server')
+    parser.add_argument('--separator', metavar='sepa', help='separator used - defaults to tab')
     args = parser.parse_args()
-    mapArgsToGloablVars(args)
+    map_args_to_global_vars(args)
 
 
-def generateLineItem(issue):
-    lineItemElements = ["status", "line", "creationDate", "fUpdateAge", "severity", "component",
+def generate_lineitem(issue):
+    lineitemelements = ["status", "line", "creationDate", "fUpdateAge", "severity", "component",
                         "rule", "project", "updateDate", "key", "message", "debt", "componentId"]
 
     body = ""
-    for element in lineItemElements:
+    for element in lineitemelements:
         try:
             if str(element) in issue:
-                body += str(issue[str(element)]) + "\t"
+
+                if element == 'component':
+                    component_string = str(issue[str(element)])
+                    components = component_string.split(":")
+                    body += components[1].title() + separator + component_string + separator
+                else:
+                    body += str(issue[str(element)]) + separator
+
             else:
-                body += " \t"
-        except (UnicodeEncodeError):
-            body += " \t"
+                body += " " + separator
+        except UnicodeEncodeError:
+            body += " " + separator
     body += " \n"
     return body
 
 
-def writeToFile(fileName, issueList):
-    print "Persisting: ", len(issueList), " issues now."
-    f = open(fileName, "wb+")
-    header = 'status\tline\tcreationDate\tfUpdateAge\tseverity\tcomponent\trule\tproject\tupdateDate\tkey\tmessage\tdebt\tcomponentId\n'
-    f.write(header)
-
-    for issue in issueList:
-        lineItem = generateLineItem(issue)
-        f.write(lineItem)
-
-        # does not work - number of rows exceeded ;)
-        # df = pandas.DataFrame(issueList)
-        # df.to_excel(fileName, sheet_name='sheet1', index=False)
+def write_to_file(file_to_write_to, issuelist):
+    for issue in issuelist:
+        line_item = generate_lineitem(issue)
+        file_to_write_to.write(line_item)
 
 ##
 ## main routine
 ##
-parseCommandlineParamters()
+parse_commandline_paramters()
 conn = httplib.HTTPSConnection(mainURL)
-result = getRuleKeys(conn)
+result = get_rulekeys(conn)
 print str(len(result)) + ' Rules to check. \n'
 
-issueList = []
-for key in result:
-    issues = getIssuesForSquid(conn, key)
-    if issues != 'nix':
-        for issue in issues:
-            issueList.append(issue)
+output_file = None
+try:
+    output_file = open(fileName, "wb+")
+except IOError:
+    print 'Could not open file - exiting'
+    exit(0)
 
-writeToFile(fileName, issueList)
-print "Done! - Collected " + str(len(issueList)) + " issues."
+header = 'status' + separator + 'line' + separator + 'creationDate' + separator + 'fUpdateAge' + separator \
+         + 'severity' + separator + 'componentName' + separator + 'component' + separator + 'rule' + separator \
+         + 'project' + separator + 'updateDate' + separator + 'key' + separator + 'message' + separator + 'debt' \
+         + separator + 'componentId\n'
+output_file.write(header)
+
+for key in result:
+    issues = get_issues_for_squid(conn, key)
+    if issues is not None:
+        write_to_file(output_file, issues)
+
+output_file.close()
+print "\n Done!"
 exit(0)
